373162ab162dcf318ba919fbb8f5bd6f4f842c77
basilkhan1250 <basilkhan1250@gmail.com>
2024-03-13 21:54:52 +0500

    Toss

--- app.js
+++ app.js
@@ -46,22 +46,23 @@
 
 
 let h1 = document.querySelector("h1")
-let image = document.querySelector(".image")
+let image = document.querySelector(".coin")
+let h3 = document.querySelector("h3")
 
 function toss(user) {
     let click = Math.ceil(Math.random() * 2)
-    // console.log(click) 
     if (click === 1) {
         image.src = "https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg"
+        h3.innerHTML = ""
     }
     else {
-        // console.log("tail")
         image.src = "https://en.numista.com/catalogue/photos/pakistan/424-original.jpg"
+        h3.innerHTML = ""
     }
-    if (user === "chand" && click === 1 || user === "masjid" && click === 2){
-        h1.innerHTML = "You win"
+    if (user === "chand" && click === 1 || user === "masjid" && click === 2) {
+        h1.innerHTML = ` It's ${user} and you win `
     }
     else {
-        h1.innerHTML = "You loss"
-     }
- }
+        h1.innerHTML = `It's ${user} and you loss`
+    }
+}
